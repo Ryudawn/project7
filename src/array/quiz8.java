e6731056b551164b4c60afb960b087554db2ff6d
user <user@DESKTOP-P99H1R2>
2023-11-01 13:46:32 +0900

    aa

--- src/array/quiz8.java
+++ src/array/quiz8.java
@@ -3,8 +3,32 @@ package array;
 public class quiz8 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Student[] info = new Student[3];
+		
+		info[0] = new Student("1","조점례");
+		info[1] = new Student("2","조훈태");
+		info[2] = new Student("3","박수철");
+		
+		for(int i = 0; i < info.length; i++) {
+		info[i].showInfo();
+		}
+		
+		
 	}
 
+}
+
+class Student {
+	String num,name;
+
+	public Student(String num, String name) {
+		this.num = num;
+		this.name = name;
+	}
+	
+	public void showInfo() {
+		System.out.println(num + " " + name );
+	}
+	
 }
